c1545aa6062393bc228e348c3433c1b936996eab
Surma <surma@surma.link>
2018-12-26 13:36:51 -0500

    Move to OOP-based encoder

--- encoder.cpp
+++ encoder.cpp
@@ -14,35 +14,113 @@
 using namespace emscripten;
 using namespace mkvmuxer;
 
-// Globals
-std::string last_error;
-uint8_t *buffer = NULL;
+#define BUFFER_SIZE 8 * 1024 * 1024
 
-int version() {
-  return VPX_CODEC_ABI_VERSION;
+int vpx_img_plane_width(const vpx_image_t *img, int plane);
+int vpx_img_plane_height(const vpx_image_t *img, int plane);
+void clear_image(vpx_image_t *img) ;
+
+class WebmEncoder {
+  public:
+    WebmEncoder(int timebase_num, int timebase_den, unsigned int width, unsigned int height, unsigned int bitrate);
+    ~WebmEncoder();
+    bool addRGBAFrame(std::string rgba);
+    val finalize();
+    std::string lastError();
+
+  private:
+    bool InitCodec(int timebase_num, int timebase_den, unsigned int width, unsigned int height, unsigned int bitrate);
+    bool InitMkvWriter();
+    bool InitImageBuffer();
+
+    bool RGBAtoVPXImage(const uint8_t *data);
+    bool EncodeFrame(vpx_image_t *img, int frame_cnt);
+    void PrintState(int line);
+
+    uint8_t *buffer;
+    vpx_codec_ctx_t ctx;
+    unsigned int frame_cnt = 0;
+    vpx_codec_enc_cfg_t cfg;
+    vpx_codec_iface_t* iface = vpx_codec_vp8_cx();
+    vpx_image_t *img;
+    std::string last_error;
+    FILE *f;
+    MkvWriter *mkv_writer;
+    Segment *main_segment;
+};
+
+#define printState() PrintState(__LINE__)
+
+void WebmEncoder::PrintState(int l) {
+  #ifndef RELEASE
+    printf(">> %d\n", l);
+    printf("Header: (0x%08x) %c%c%c%c\n", (int) buffer, buffer[0], buffer[1], buffer[2], buffer[3]);
+    printf("File: 0x%08x\n", (int)f);
+    printf("last_errror: %s\n", last_error.c_str());
+    printf("frame_cnt: %d\n", frame_cnt);
+    printf("mkv_writer: 0x%08x\n", (int) mkv_writer);
+    printf("main_segment: 0x%08x\n", (int) main_segment);
+  #endif
 }
 
-void generate_frame(uint8_t *img, int frame) {
-  int stride = 300 * 4;
-  auto *row = img;
+WebmEncoder::WebmEncoder(int timebase_num, int timebase_den, unsigned int width, unsigned int height, unsigned int bitrate) {
+  buffer = (uint8_t*) malloc(BUFFER_SIZE);
+  memset(buffer, 0, BUFFER_SIZE);
 
-  row += stride * frame;
-
-  for(int y = 0; y < 10; y++) {
-    for(int x = 0; x < 10; x++) {
-      row[(frame + x) * 4 + 1 + (frame % 3)] = 255;
-    }
-    row += stride;
+  if(!InitCodec(timebase_num, timebase_den, width, height, bitrate)) {
+    return;
   }
+  if(!InitMkvWriter()) {
+    return;
+  }
+  if(!InitImageBuffer()) {
+    return;
+  }
+  printState();
 }
 
-bool encode_frame(vpx_codec_ctx_t *ctx, vpx_image_t *img, Segment *segment, int frame_cnt) {
+WebmEncoder::~WebmEncoder() {
+  vpx_img_free(img);
+  delete main_segment;
+  delete mkv_writer;
+  delete buffer;
+}
+
+bool WebmEncoder::addRGBAFrame(std::string rgba) {
+  RGBAtoVPXImage((const uint8_t*) rgba.c_str());
+  return EncodeFrame(img, frame_cnt++);
+}
+
+
+val WebmEncoder::finalize() {
+  printState();
+  if(!EncodeFrame(NULL, -1)) {
+    last_error = "Could not encode flush frame";
+    return val::undefined();
+  }
+
+  printState();
+  if(!main_segment->Finalize()) {
+    last_error = "Could not finalize mkv";
+    return val::undefined();
+  }
+  fflush(f);
+  auto len = mkv_writer->Position();
+  printState();
+  return val(typed_memory_view(len, buffer));
+}
+
+std::string WebmEncoder::lastError() {
+  return std::string(last_error);
+}
+
+bool WebmEncoder::EncodeFrame(vpx_image_t *img, int frame_cnt) {
   vpx_codec_iter_t iter = NULL;
   const vpx_codec_cx_pkt_t *pkt;
   vpx_codec_err_t err;
 
   err = vpx_codec_encode(
-    ctx,
+    &ctx,
     img,
     frame_cnt, /* time of frame */
     1, /* length of frame */
@@ -53,22 +131,113 @@ bool encode_frame(vpx_codec_ctx_t *ctx, vpx_image_t *img, Segment *segment, int 
     last_error = std::string(vpx_codec_err_to_string(err));
     return false;
   }
-  while((pkt = vpx_codec_get_cx_data(ctx, &iter)) != NULL) {
+  while((pkt = vpx_codec_get_cx_data(&ctx, &iter)) != NULL) {
     if(pkt->kind != VPX_CODEC_CX_FRAME_PKT) {
       continue;
     }
     auto frame_size = pkt->data.frame.sz;
     auto is_keyframe = (pkt->data.frame.flags & VPX_FRAME_IS_KEY) != 0;
-    if(!segment->AddFrame(
+    if(!main_segment->AddFrame(
       (uint8_t*) pkt->data.frame.buf,
       pkt->data.frame.sz,
       1, /* track id */
-      pkt->data.frame.pts * 1000000000ULL/30,
+      pkt->data.frame.pts * 1000000000ULL * cfg.g_timebase.num/cfg.g_timebase.den,
       is_keyframe
     )) {
       last_error = "Could not add frame";
       return false;
     }
+  }
+  printState();
+  return true;
+}
+
+bool WebmEncoder::InitCodec(int timebase_num, int timebase_den, unsigned int width, unsigned int height, unsigned int bitrate) {
+  vpx_codec_err_t err;
+  err = vpx_codec_enc_config_default(iface, &cfg, 0);
+  if(err != VPX_CODEC_OK) {
+    last_error = std::string(vpx_codec_err_to_string(err));
+    return false;
+  }
+
+  cfg.g_timebase.num = timebase_num;
+  cfg.g_timebase.den = timebase_den;
+  cfg.g_w = width;
+  cfg.g_h = height;
+  cfg.rc_target_bitrate = bitrate;
+
+  err = vpx_codec_enc_init(
+    &ctx,
+    iface,
+    &cfg,
+    0 /* flags */
+  );
+  if(err != VPX_CODEC_OK) {
+    last_error = std::string(vpx_codec_err_to_string(err));
+    return false;
+  }
+  return true;
+}
+
+bool WebmEncoder::InitMkvWriter() {
+  f = fmemopen(buffer, BUFFER_SIZE, "wb");
+  mkv_writer = new MkvWriter(f);
+  main_segment = new Segment();
+  if(!main_segment->Init(mkv_writer)) {
+    last_error = "Could not initialize main segment";
+    return false;
+  }
+  printState();
+  if(main_segment->AddVideoTrack(cfg.g_w, cfg.g_h, 1 /* track id */) == 0) {
+    last_error = "Could not add video track";
+    return false;
+  }
+  main_segment->set_mode(Segment::Mode::kFile);
+  auto info = main_segment->GetSegmentInfo();
+  // Branding, yo
+  auto muxing_app = std::string(info->muxing_app()) + " but in wasm";
+  auto writing_app = std::string(info->writing_app()) + " but in wasm";
+  info->set_writing_app(writing_app.c_str());
+  info->set_muxing_app(muxing_app.c_str());
+  return true;
+}
+
+bool WebmEncoder::InitImageBuffer() {
+  img = vpx_img_alloc(
+    NULL, /* allocate buffer on the heap */
+    VPX_IMG_FMT_I420,
+    cfg.g_w,
+    cfg.g_h,
+    0 /* align. simple_encoder says 1? */
+  );
+  if(img == NULL) {
+    last_error = "Could not allocate vpx image buffer";
+    return false;
+  }
+  return true;
+}
+
+bool WebmEncoder::RGBAtoVPXImage(const uint8_t *rgba) {
+  clear_image(img);
+  // litten endian BGRA means it is ARGB in memory
+  if(
+    libyuv::BGRAToI420(
+      // Since we are ignoring opacity anyways (currently), we can use
+      // this nasty nasty trick to avoid reshuffling the entire memory
+      // from RGBA to ARGB.
+      rgba-1,
+      cfg.g_w*4,
+      img->planes[VPX_PLANE_Y],
+      img->stride[VPX_PLANE_Y],
+      img->planes[VPX_PLANE_U],
+      img->stride[VPX_PLANE_U],
+      img->planes[VPX_PLANE_V],
+      img->stride[VPX_PLANE_V],
+      cfg.g_w,
+      cfg.g_h
+    ) != 0) {
+    last_error = "Could not convert to I420";
+    return false;
   }
   return true;
 }
@@ -103,131 +272,15 @@ void clear_image(vpx_image_t *img) {
   }
 }
 
-#define BUFFER_SIZE 8 * 1024 * 1024
-
-val encode() {
-  vpx_codec_ctx_t ctx;
-  auto iface = vpx_codec_vp8_cx();
-  vpx_codec_enc_cfg_t cfg;
-  vpx_codec_err_t err;
-
-  err = vpx_codec_enc_config_default(iface, &cfg, 0);
-  if(err != VPX_CODEC_OK) {
-    last_error = std::string(vpx_codec_err_to_string(err));
-    return val::undefined();
-  }
-
-  cfg.g_timebase.num = 1;
-  cfg.g_timebase.den = 30; // = fps
-  cfg.g_w = 300;
-  cfg.g_h = 300;
-  cfg.rc_target_bitrate = 200; // FIXME
-
-  err = vpx_codec_enc_init(
-    &ctx,
-    iface,
-    &cfg,
-    0 /* flags */
-  );
-  if(err != VPX_CODEC_OK) {
-    last_error = std::string(vpx_codec_err_to_string(err));
-    return val::undefined();
-  }
-
-  buffer = (uint8_t*) malloc(BUFFER_SIZE);
-  if(buffer == NULL) {
-    last_error = "Could not allocate buffer";
-    return val::undefined();
-  }
-  memset(buffer, 0, BUFFER_SIZE);
-  auto f = fmemopen(buffer, BUFFER_SIZE, "wb");
-  auto mkv_writer = new MkvWriter(f);
-  auto main_segment = new Segment();
-  if(!main_segment->Init(mkv_writer)) {
-    last_error = "Could not initialize main segment";
-    return val::undefined();
-  }
-  if(main_segment->AddVideoTrack(cfg.g_w, cfg.g_h, 1 /* track id */) == 0) {
-    last_error = "Could not add video track";
-    return val::undefined();
-  }
-  main_segment->set_mode(Segment::Mode::kFile);
-  auto info = main_segment->GetSegmentInfo();
-  // BRANDING yo
-  auto muxing_app = std::string(info->muxing_app()) + " but in wasm";
-  auto writing_app = std::string(info->writing_app()) + " but in wasm";
-  info->set_writing_app(writing_app.c_str());
-  info->set_muxing_app(muxing_app.c_str());
-
-  vpx_image_t *img = vpx_img_alloc(
-    NULL, /* allocate buffer on the heap */
-    VPX_IMG_FMT_I420,
-    300,
-    300,
-    0 /* align. simple_encoder says 1? */
-  );
-  if(img == NULL) {
-    last_error = "Could not allocate vpx image buffer";
-    return val::undefined();
-  }
-
-  uint8_t img_buffer[300 * 300 * 4];
-  for(int i = 0; i < 100; i++) {
-    clear_image(img);
-    memset(img_buffer, 0, 300 * 300 * 4);
-    generate_frame(img_buffer, i);
-    // litten endian BGRA means it is ARGB in memory
-    if(
-      libyuv::BGRAToI420(
-        img_buffer,
-        300*4,
-        img->planes[VPX_PLANE_Y],
-        img->stride[VPX_PLANE_Y],
-        img->planes[VPX_PLANE_U],
-        img->stride[VPX_PLANE_U],
-        img->planes[VPX_PLANE_V],
-        img->stride[VPX_PLANE_V],
-        300,
-        300
-      ) != 0) {
-        last_error = "Could not convert to I420";
-        return val::undefined();
-      }
-    if(!encode_frame(&ctx, img, main_segment, i)) {
-      last_error = "Could not encode frame";
-      return val::undefined();
-    }
-  }
-  vpx_img_free(img);
-
-  // Flush
-  if(!encode_frame(&ctx, NULL, main_segment, -1)) {
-    last_error = "Could not encode flush frame";
-    return val::undefined();
-  }
-
-  if(!main_segment->Finalize()) {
-    last_error = "Could not finalize mkv";
-    return val::undefined();
-  }
-  fflush(f);
-  auto len = mkv_writer->Position();
-  delete main_segment;
-  delete mkv_writer;
-  return val(typed_memory_view(len, buffer));
-}
-
-val last_error_desc() {
-  return val(last_error);
-}
-
-void free_buffer() {
-  free(buffer);
-}
-
 EMSCRIPTEN_BINDINGS(my_module) {
-  function("version", &version);
-  function("encode", &encode);
-  function("free_buffer", &free_buffer);
-  function("last_error_desc", &last_error_desc);
+  // function("abi_version", &abi_version);
+  // function("encode", &encode);
+  // function("free_buffer", &free_buffer);
+  // function("last_error_desc", &last_error_desc);
+
+  class_<WebmEncoder>("WebmEncoder")
+    .constructor<int, int, unsigned int, unsigned int, unsigned int>()
+    .function("addRGBAFrame", &WebmEncoder::addRGBAFrame)
+    .function("finalize", &WebmEncoder::finalize)
+    .function("lastError", &WebmEncoder::lastError);
 }
